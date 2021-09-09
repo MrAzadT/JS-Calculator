c187f9423b1f54062c16b92e2ff4b275d46a48a4
azadtamal <mrazadt@gmail.com>
2021-09-09 20:11:32 +0600

    class

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
 // let str = "";
 // console.log(!!str);
 
-let r = /azad/;
-console.log(r);
-console.log(r.source);
+// let r = /azad/;
+// console.log(r);
+// console.log(r.source);
+
+class person {
+  constructor() {
+    this.name = "azad";
+  }
+}
+const person2 = new person();
+console.log(person2.name);
