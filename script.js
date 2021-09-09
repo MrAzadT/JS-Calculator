a19e1bcd789b287ef3722e320f3694659d6d6564
azadtamal <mrazadt@gmail.com>
2021-09-09 20:23:44 +0600

    class

--- script.js
+++ script.js
@@ -6,11 +6,15 @@
 // console.log(r.source);
 
 class person {
-  constructor(name1) {
+  constructor(name1, age) {
     this.name1 = name1;
+    this.age = age;
+  }
+  greed() {
+    return `my name  ${this.name1}  and my age  ${this.age}`;
   }
 }
-const person2 = new person("azad");
-const person3 = new person("tamal");
-console.log(person2.name1);
-console.log(person3.name1);
+const person2 = new person("azad", 29);
+const person3 = new person("tamal", 35);
+console.log(person2.greed());
+// console.log(person3.greed());
