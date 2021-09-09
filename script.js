9213780c9b0b11c98f3270a1fd1fd06e16623d25
azadtamal <mrazadt@gmail.com>
2021-09-09 20:35:12 +0600

    class

--- script.js
+++ script.js
@@ -5,16 +5,28 @@
 // console.log(r);
 // console.log(r.source);
 
+// class person {
+//   constructor(name1, age) {
+//     this.name1 = name1;
+//     this.age = age;
+//   }
+//   greed() {
+//     return `my name  ${this.name1}  and my age  ${this.age}`;
+//   }
+// }
+// const person2 = new person("azad", 29);
+// const person3 = new person("tamal", 35);
+// console.log(person2.greed());
+// // console.log(person3.greed());
+
 class person {
-  constructor(name1, age) {
-    this.name1 = name1;
+  constructor(name, age) {
+    this.name = name;
     this.age = age;
   }
-  greed() {
-    return `my name  ${this.name1}  and my age  ${this.age}`;
+  info() {
+    return `hiii i am ${this.name} and my age ${this.age}`;
   }
 }
-const person2 = new person("azad", 29);
-const person3 = new person("tamal", 35);
-console.log(person2.greed());
-// console.log(person3.greed());
+const per = new person("azad", 26);
+console.log(per.info());
