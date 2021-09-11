3d3b9e97b5f1b94fc12779a93c4ba72bb2ce39d9
azadtamal <mrazadt@gmail.com>
2021-09-11 15:12:35 +0600

    calculator

--- script.js
+++ script.js
@@ -37,7 +37,14 @@
 // const person = { name, age };
 // console.log(person);
 
-const adventurer = { name: "Alice", cat: { name: "Lucy" } };
+// const adventurer = { name: "Alice", cat: { name: "Lucy" } };
 
-const ratName = adventurer?.rat?.name;
-console.log(ratName);
+// const ratName = adventurer?.rat?.name;
+// console.log(ratName);
+// let person = null;
+
+// console.log(person ? person : "person is null");
+
+const obj = { a: 1 };
+
+console.log(Object.keys(obj).length === 0);
